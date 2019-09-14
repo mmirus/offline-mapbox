fabcb5a5f3fe458049e042fe69d3744288265764
Matt Mirus <matt@mattmirus.com>
2019-09-14 15:18:13 -0400

    Prompt user to reload when an update is available

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
